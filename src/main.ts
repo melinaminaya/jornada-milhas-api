d2ecd2b1b8822e849d73082b5b28beea89d00be4
Melina Minaya <melina.minaya@autotrac.com.br>
2024-09-18 10:12:55 -0300

    Push do api de suporte

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:4200', // Your Angular app URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200,
+  });
 
   await app.listen(8080);
 }
